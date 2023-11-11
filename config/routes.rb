9f3e77e878a3a21431f5f838bf03106759cd1040
jecode93 <jeanemmanuelcadet@gmail.com>
2023-11-10 22:26:15 -0500

    Set up the application home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "articles#index"
+  
   get "/articles", to: "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

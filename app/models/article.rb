58b64eeb6bbcffbda20f3b7b87928c8825f75f06
jecode93 <jeanemmanuelcadet@gmail.com>
2023-11-11 00:28:51 -0500

    Create a new article, add validations and display error message if there is error from the field to fill.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,17 @@
 class Article < ApplicationRecord
+	# Validations are rules that are checked before a model object is saved. 
+	# If any of the checks fail, the save will be aborted, 
+	# and appropriate error messages will be added to the errors attribute 
+	# of the model object.
+	
+	# The first validation declares that a title value must be present. 
+	# Because title is a string, this means that the title value must 
+	# contain at least one non-whitespace character.
+
+	# The second validation declares that a body value must also be present. 
+	# Additionally, it declares that the body value must be at least 10 
+	# characters long.
+	 
+	validates :title, presence: true
+	validates :body, presence: true, length: { minimum: 10 }
 end
